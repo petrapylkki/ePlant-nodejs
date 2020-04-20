d10df287c3cb5b8c53844515020ea320e7c1c9c2
Jaakko Kammonen <jaakko.kammonen@hotmail.com>
2020-04-20 12:44:21 +0300

    päivittetty

--- frontend/src/components/vesipumppu.js
+++ frontend/src/components/vesipumppu.js
@@ -1,9 +1,41 @@
+import { View } from "react-native"
 
 
 
 
+export default function vesipumppu() {
+
+let vesipaalle = "api.thingspeak.com/update?api_key=7NFTTNLSIK37HNCC&field3=1";
+let vesipois = "api.thingspeak.com/update?api_key=7NFTTNLSIK37HNCC&field3=0";
+
+fetch('https://mywebsite.com/endpoint/', {
+  method: 'POST',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    firstParam: 'yourValue',
+    secondParam: 'yourOtherValue',
+  }),
+});
+    
+
+return(
+<View>
 
 
+</View>
 
-    let vesipaalle = "api.thingspeak.com/update?api_key=7NFTTNLSIK37HNCC&field3=1"
-    let vesipois = "api.thingspeak.com/update?api_key=7NFTTNLSIK37HNCC&field3=0"
+);
+
+
+}
+
+const styles = StyleSheet.create({
+
+    button: {
+        backgroundColor: '#FCFCFC',
+        flex: 1
+    }
+});
