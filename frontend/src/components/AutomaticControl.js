a25a97785bbdabd2d00e5bb616e85b9a8873a321
Minna Kuivalainen <bgg244@myy.haaga-helia.fi>
2020-05-10 21:16:05 +0300

    code commented

--- frontend/src/components/AutomaticControl.js
+++ frontend/src/components/AutomaticControl.js
@@ -6,27 +6,32 @@ export default function AutomaticControl() {
   const [res, setRes] = React.useState(0);
   const [isEnabled, setIsEnabled] = React.useState(false);
 
+  // define ThingSpeak API-key
   const apikey = "XR2Z3K3KE1Q1V3UA";
 
-  const AutomaticOn = () => {
-      const url = 'https://api.thingspeak.com/update?api_key=' + apikey+ '&field5=1';
-      fetch(url)
-      .then((response) => response.json())
-      .then((responseJson) => {
-          while (true) {
-              if (parseInt(responseJson) == 0) { 
-                  AutomaticOn();
-                  console.log(responseJson);
-              } 
-              break; 
-          }; 
-          setRes(responseJson);
-      })
-      .catch((error) => {
-        Alert.alert('Error', error);
-      });
-  }
+    // goes through the conditional statement until the desired value is obtained from ThingSpeak
+    // after this AutomaticOn -function launched
+    const AutomaticOn = () => {
+        const url = 'https://api.thingspeak.com/update?api_key=' + apikey+ '&field5=1';
+        fetch(url)
+        .then((response) => response.json())
+        .then((responseJson) => {
+            while (true) {
+                if (parseInt(responseJson) == 0) { 
+                    AutomaticOn();
+                    console.log(responseJson);
+                } 
+                break; 
+            }; 
+            setRes(responseJson);
+        })
+        .catch((error) => {
+            Alert.alert('Error', error);
+        });
+    }
 
+    // goes through the conditional statement until the desired value is obtained from ThingSpeak
+    // after this AutomaticOff -function launched
     const AutomaticOff = () => {
       const url = 'https://api.thingspeak.com/update?api_key=' + apikey+ '&field5=0';
       fetch(url)
@@ -47,23 +52,24 @@ export default function AutomaticControl() {
 
 }
 
+    // handles toggle switch component 
+    const toggleSwitch = () => {
+        // setting the isEnabled function
+        setIsEnabled(previousState => !previousState);
+        
+        // goes in reverse order, False is on, and True is off
+        if (isEnabled == false) {
+            console.log("Automatic-mode is on")
+            AutomaticOn();
 
-const toggleSwitch = () => {
-    
-    setIsEnabled(previousState => !previousState);
-    
-    // These goes in reverse order! False is on, and true is off
-    if (isEnabled == false) {
-        console.log("Automatic-mode is on")
-        AutomaticOn();
-
+        }
+        else if (isEnabled == true) {
+            console.log("Automatic-mode is off");
+            AutomaticOff();
+        }
     }
-    else if(isEnabled == true) {
-        console.log("Automatic-mode is off");
-        AutomaticOff();
-    }
-}
 
+// returns toggle switch-button on the screen
 return(
 
   <View style={styles.container}>
