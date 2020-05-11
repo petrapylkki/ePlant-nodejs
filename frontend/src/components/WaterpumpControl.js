88b4190e4d7c95db4d72a0f730cbc21a11eaebce
Minna Kuivalainen <bgg244@myy.haaga-helia.fi>
2020-05-11 12:40:34 +0300

    buttonin tyylit toimii

--- frontend/src/components/WaterpumpControl.js
+++ frontend/src/components/WaterpumpControl.js
@@ -1,16 +1,14 @@
 // importing all necessary components
 import React, { useState } from 'react';
 import { View, Button, StyleSheet } from 'react-native';
-import { DotIndicator } from 'react-native-indicators';
 
 export default function waterpumpControl() {
   const [res, setRes] = useState(0);
-  const [isLoading, setLoading] = useState(false);
 
   // define ThingSpeak API-key
   const apikey = "XR2Z3K3KE1Q1V3UA";
 
-  // handles waterpump ON-functionality asynchronously
+  // handles waterpump ON-functionality
   // goes through the conditional statement until the desired value is obtained from ThingSpeak
   // after this WaterOn -function launched
   const waterOn = () => {
@@ -32,7 +30,7 @@ export default function waterpumpControl() {
     });
 }
 
-  // handles waterpump OFF-functionality asynchronously
+  // handles waterpump OFF-functionality
   // goes through the conditional statement until the desired value is obtained from ThingSpeak
   // after this WaterOff -function launched
   const waterOff = () => {
@@ -55,7 +53,7 @@ export default function waterpumpControl() {
 
 }
 
-
+// waits 20 seconds when watering
   const wait = (ms) => {
     let start = new Date().getTime();
     let end = start;
@@ -89,33 +87,41 @@ return(
         title="Water the plant"
         style={styles.button}
         />  
-    <DotIndicator
-         animating={isLoading}
-         color='#63816D'
-         hidesWhenStopped={true}
-       />
   </View>
 );
 }
 
 const styles = StyleSheet.create({
-
-    box: {
-      width:150,
-      marginLeft:35,
-      marginTop:15,
-      color: '#63816D',
-      backgroundColor: "#c1dbc9",
-      fontSize: 15,
-      fontWeight: "bold",
-      textAlign:"center",
-      borderColor: "#c1dbc9",
-      borderWidth: 1,
-      borderRadius:10,
-      padding:4,
-      margin: 7,
-      width:150
-    },
+  box: {
+    width:150,
+    marginLeft:35,
+    marginTop:15,
+    color: '#63816D',
+    backgroundColor: "#c1dbc9",
+    fontSize: 15,
+    fontWeight: "bold",
+    textAlign:"center",
+    borderColor: "#c1dbc9",
+    borderWidth: 1,
+    borderRadius:10,
+    padding:4,
+    margin: 7,
+    width:150
+  },
+  waterpumpButton: {
+    color: '#63816D',
+    backgroundColor: "#c1dbc9",
+    fontSize: 15,
+    fontWeight: "bold",
+    textAlign:"center",
+    borderColor: "#c1dbc9",
+    borderWidth: 1,
+    borderRadius:10,
+    padding:4,
+    marginLeft: 7,
+    width:150,
+    marginTop: 10
+  },
     loadingIndicator: {
       justifyContent: 'center',
 
